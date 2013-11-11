d26f24ae2ece5ba5b98addbb4c14913fe74ab780
kalle936 <kalle936@hotmail.com>
2013-11-11 14:45:13 +0100

    Windows

--- RockPaperScissor/src/rockpaperscissor/RockPaperScissor.java
+++ RockPaperScissor/src/rockpaperscissor/RockPaperScissor.java
@@ -1,13 +1,27 @@
 package rockpaperscissor;
 
+import java.io.IOException;
+import java.util.*;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
- * @author miikka
+ * @author miikka   
  */
 public class RockPaperScissor {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Choose Port: ");
+        int port = in.nextInt();
+        try {
+            ServerSocket servsocket = new ServerSocket(port);
+            Peer peer = new Peer (servsocket);
+        } catch (IOException ex) {
+            Logger.getLogger(RockPaperScissor.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
