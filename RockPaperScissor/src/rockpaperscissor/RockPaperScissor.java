af8fdb828df60fe33665965663507dc1b585d546
kalle936 <kalle936@hotmail.com>
2013-11-14 13:19:33 +0100

    Modified interface actions

--- RockPaperScissor/src/rockpaperscissor/RockPaperScissor.java
+++ RockPaperScissor/src/rockpaperscissor/RockPaperScissor.java
@@ -5,6 +5,7 @@ import java.util.*;
 import java.net.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import userinterface.ConnectWindow;
 /**
  *
  * @author miikka   
@@ -12,22 +13,24 @@ import java.util.logging.Logger;
 public class RockPaperScissor {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Choose Port to listen to new connections: ");
-        int port = in.nextInt();
-        try {
-            Peer peer = startServer(port);
-        
-        //TEST    
-        testConnect(peer);    
-        //TEST
-            
-        //TEST
-        sendTestMessage(peer);
-        //TEST
-        } catch (IOException ex) {
-            Logger.getLogger(RockPaperScissor.class.getName()).log(Level.SEVERE, null, ex);
-        }
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Choose Port to listen to new connections: ");
+//        int port = in.nextInt();
+//        try {
+//            Peer peer = startServer(port);
+//        
+//        //TEST    
+//        testConnect(peer);    
+//        //TEST
+//            
+//        //TEST
+//        sendTestMessage(peer);
+//        //TEST
+//        } catch (IOException ex) {
+//            Logger.getLogger(RockPaperScissor.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        ConnectWindow cw = new ConnectWindow();
+        ConnectWindow.startConnectWindow();
     }
     
     public static Peer startServer(int port) throws IOException {
